5224601dd24487fe744777602f7333e266fe91e7
Bruno Gonzalez <bgonzalez@idatha.com>
2019-01-07 00:48:23 -0300

    Mejoras y cambios

--- js/main.js
+++ js/main.js
@@ -226,16 +226,45 @@ $('.select-wrap').on('click', 'select', function() {
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map_canvas'), {
-		center: { lat: -34.736318, lng: -56.269478 },
+		center: { lat: -34.731318, lng: -56.269478 },
 		zoom: 13,
 		scrollwheel: false,
 		mapTypeControl: false
+	});
+	// var contentString = '<div id="content">' +
+	// 	'<div id="siteNotice">' +
+	// 	'</div>' +
+	// 	'<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+	// 	'<div id="bodyContent">' +
+	// 	'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+	// 	'sandstone rock formation in the southern part of the ' +
+	// 	'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+	// 	'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+	// 	'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+	// 	'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+	// 	'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+	// 	'Aboriginal people of the area. It has many springs, waterholes, ' +
+	// 	'rock caves and ancient paintings. Uluru is listed as a World ' +
+	// 	'Heritage Site.</p>' +
+	// 	'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+	// 	'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+	// 	'(last visited June 22, 2009).</p>' +
+	// 	'</div>' +
+	// 	'</div>';
+
+	var contentString = 'Boda Tavo & Anita';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
 	});
 	var image = 'img/point.png';
 	var weddingMarker = new google.maps.Marker({
 		position: { lat: -34.718534, lng: -56.290802 },
 		map: map,
 		icon: image
+	});
+	weddingMarker.addListener('click', function () {
+		infowindow.open(map, weddingMarker);
 	});
 }
 /* 8. Gifts list
